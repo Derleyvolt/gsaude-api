8694f2c55dfb5e1a0f4818718285688b9851af49
AdrianAraujo <adrianaraujo247@gmail.com>
2022-06-05 11:43:46 -0300

    Finalizando a busca de um remedio

--- controllers/medicine.js
+++ controllers/medicine.js
@@ -4,11 +4,26 @@ const medicineModel = require("../models/medicine")
 const getMedicine = async (req,res) => {
   try {
     const select = "name latitude longitude medicines"
-    let medicine = await medicineModel.findOne({ name: req.params.name })
+    let medicineInfo = await medicineModel.findOne({ name: req.params.name })
       .populate({path:"inventory", select})
 
-    // medicine.invetory.medicines = medicine.filter((element) => element.invetory.medicines.medicine == medicine._id)
-    res.status(200).json(medicine)
+    if(medicineInfo != null) {
+      medicineInfo = medicineInfo.toObject()
+      delete medicineInfo.updatedAt
+
+      medicineInfo.inventory.forEach(element => {
+        const arrayMedicines = element.medicines.filter(item => item.medicine.equals(medicineInfo._id))
+        element.medicine = arrayMedicines[0].medicine
+        element.amountAvailable = arrayMedicines[0].amountAvailable
+        element.situation = arrayMedicines[0].situation
+        delete element.medicines
+      })
+      res.status(200).json(medicineInfo)
+    }else {
+      res.status(200).json({message: "Remédio indisponível nos postos"})
+    }
+
+    
 
   }catch(err) {
     res.status(500).json(err)
